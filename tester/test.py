5bebc32500d6a4aa4c237e4a606bfc25be8d382e
Cem koyluoglu <61415601+CemRoot@users.noreply.github.com>
2022-10-10 21:18:31 +0300

    Build Your Own REST API Service

---
+++ tester/test.py
@@ -0,0 +1,19 @@
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = input()
+t = input()
+
+# Write an answer using print
+# Input
+# First Line: An Integer N
+# Second Line: A String S
+
+for i in range(int(n)):
+	print(t)
+
+
+
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+print("answer")
